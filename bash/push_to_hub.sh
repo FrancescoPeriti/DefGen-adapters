c21442b69b480f955a30f91e2a83b73b8e68e6b7
Francesco Periti <44090706+FrancescoPeriti@users.noreply.github.com>
2025-07-08 11:31:09 +0200

    Create push_to_hub.sh

---
+++ bash/push_to_hub.sh
@@ -0,0 +1,125 @@
+# CA
+model_path="models/LlamaDictionary-ca_ML38BI/checkpoint-2769"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-ca_ML38BI"
+
+# LA
+model_path="models/LlamaDictionary-la_ML38BI/checkpoint-38"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-la_ML38BI"
+
+# LT
+model_path="models/LlamaDictionary-lt_ML38BI/checkpoint-48"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-lt_ML38BI"
+
+# DA
+model_path="models/LlamaDictionary-da_ML38BI/checkpoint-85"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-da_ML38BI"
+
+# MG
+model_path="models/LlamaDictionary-mg_ML38BI/checkpoint-980"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-mg_ML38BI"
+
+# DE
+model_path="models/LlamaDictionary-de_ML38BI/checkpoint-64640"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-de_ML38BI"
+
+# NL
+model_path="models/LlamaDictionary-nl_ML38BI/checkpoint-25110"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-nl_ML38BI"
+
+# EL
+model_path="models/LlamaDictionary-el_ML38BI/checkpoint-3552"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-el_ML38BI"
+
+# EN
+model_path="models/LlamaDictionary-en_ML38BI/checkpoint-128238"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-en_ML38BI"
+
+# ES
+model_path="models/LlamaDictionary-es_ML38BI/checkpoint-3565"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-es_ML38BI"
+
+# NO
+model_path="models/LlamaDictionary-no_ML38BI/checkpoint-900"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-no_ML38BI"
+
+# FI
+model_path="models/LlamaDictionary-fi_ML38BI/checkpoint-5565"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-fi_ML38BI"
+
+# PL
+model_path="models/LlamaDictionary-pl_ML38BI/checkpoint-12804"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-pl_ML38BI"
+
+# FR
+model_path="models/LlamaDictionary-fr_ML38BI/checkpoint-121100"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-fr_ML38BI"
+
+# PT
+model_path="models/LlamaDictionary-pt_ML38BI/checkpoint-1020"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-pt_ML38BI"
+
+# RU
+model_path="models/LlamaDictionary-ru_ML38BI/checkpoint-19776"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-ru_ML38BI"
+
+# IT
+model_path="models/LlamaDictionary-it_ML38BI/checkpoint-1200"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-it_ML38BI"
+
+# SV
+model_path="models/LlamaDictionary-sv_ML38BI/checkpoint-12038"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-sv_ML38BI"
+
+# TR
+model_path="models/LlamaDictionary-sv_ML38BI/checkpoint-2176"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-tr_ML38BI"
+
+# ZH
+model_path="models/LlamaDictionary-zh_ML38BI/checkpoint-1470"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-zh_ML38BI"
+
+# JA
+model_path="models/LlamaDictionary-ja_ML38BI/checkpoint-2376"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-ja_ML38BI"
+
+# KU
+model_path="models/LlamaDictionary-ku_ML38BI/checkpoint-2188"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-ku_ML38BI"
+
+
+
+# PL-RU
+model_path="models/LlamaDictionary-pl ru_ML38BI/checkpoint-1148"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-pl-ru_ML38BI"
+
+# PL-SV-RU-DE-EN
+model_path="models/LlamaDictionary-pl sv ru de en_ML38BI/checkpoint-1360"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-pl-sv-ru-de-en_ML38BI"
+
+# IT-ES-FR
+model_path="models/LlamaDictionary-it es fr_ML38BI/checkpoint-1236"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-it-es-fr_ML38BI"
+
+# IT-ES-PL-SV-RU-DE-FR-EN
+model_path="models/LlamaDictionary-it es pl sv ru de fr en_ML38BI/checkpoint-3279"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-it-es-pl-sv-ru-de-fr-en_ML38BI"
+
+# IT-ES-PL-RU-FR
+model_path="models/LlamaDictionary-it es pl ru fr_ML38BI/checkpoint-1400"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-it-es-pl-ru-fr_ML38BI"
+
+# IT-ES-SV-DE-FR-EN
+model_path="models/LlamaDictionary-it es sv de fr en_ML38BI/checkpoint-1612"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-it-es-sv-de-fr-en_ML38BI"
+
+# IT-ES-TR-JA-EL-FI-KU-PL-SV-RU-DE-FR-EN
+model_path="models/LlamaDictionary-it es tr ja el fi ku pl sv ru de fr en_ML38BI/checkpoint-7496"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-it-es-tr-ja-el-fi-ku-pl-sv-ru-de-fr-en_ML38BI"
+
+# SV-DE-EN
+model_path="models/LlamaDictionary-sv de en_ML38BI/checkpoint-1179"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-sv-de-en_ML38BI"
+
+# JA-FI-RU
+model_path="models/LlamaDictionary-ja fi ru_ML38BI/checkpoint-3408"
+sbatch --time=00:40:00 bash/_push_to_hub.sh "$model_path" "LlamaDictionary-ja-fi-ru_ML38BI"
